578aff1f6b34e6ce484176142a5448ded7d5d746
Darzox221 <dia9amadu@gmail.com>
2026-04-24 22:55:06 +0000

    mise à jour script et style

--- script.js
+++ script.js
@@ -66,12 +66,14 @@ const navMenu = document.querySelector('.nav-menu');
 hamburger.addEventListener('click', () => {
   hamburger.classList.toggle('active');
   navMenu.classList.toggle('active');
+  document.body.classList.toggle('menu-open');
 });
 
 document.querySelectorAll('.nav-link').forEach(link => {
   link.addEventListener('click', () => {
     hamburger.classList.remove('active');
     navMenu.classList.remove('active');
+    document.body.classList.remove('menu-open');
   });
 });
 
